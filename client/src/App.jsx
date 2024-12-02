df92ab0b504ad0bedd8d70d5768b3e721e34106d
Harshi <94774731+harshi-s@users.noreply.github.com>
2024-12-02 15:56:28 +0530

    Add files via upload

---
+++ client/src/App.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react';
+import './App.css';
+import { Outlet } from 'react-router-dom';
+import { Navbar } from './components/common/Navbar';
+import { MyFooter } from './components/common/MyFooter';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <div className="min-h-screen">
+        <Outlet />
+      </div>
+      <MyFooter />
+    </>
+  );
+}
+
+export default App;
+
+
+
+
+
+
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+// import { Outlet } from 'react-router-dom'
+// import { Navbar } from './components/common/Navbar'
+// import {MyFooter} from './components/common/MyFooter'
+
+
+// function App() {
+//   const [count, setCount] = useState(0)
+  
+
+//   return (
+//     <>
+//       <Navbar/>
+//       <div className='min-h-screen'>
+//         <Outlet/>
+//       </div>
+//       <MyFooter/>
+//     </>
+//   )
+// }
+
+// export default App
